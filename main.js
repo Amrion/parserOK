391a6d56037a01fce56ce4ddd42e1ca20239cd99
Danya <danyat02@yandex.ru>
2023-05-23 20:13:52 +0300

    Add full notes

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 import {openBrowser, closeBrowser} from "./browserWork.js";
 import {parse} from "./parser.js";
 
-const URL = 'https://ok.ru/profile208491525/statuses';
+const URL = 'https://ok.ru/profile/513703463805/statuses';
 const LOGIN = '';
 const PASS = '';
 
 const browser = await openBrowser();
 const result = await parse( browser, LOGIN, PASS, URL);
 
+
 console.log(result.logs)
 if (result.length !== undefined) {
     console.log(result.length)
-// Вывод массив заметок - console.log(result.array)
+    console.log(result.array)
 }
 
 await closeBrowser(browser);
--- parser.js
+++ parser.js
@@ -19,13 +19,13 @@ export const parse = async (browser, login, password, url) => {
 
     await page.click('.button__pe9qo.button-core-container__0ej09.js-submit.__wide__pe9qo')
 
-    return await new Promise((resolve) => {
+    const href = await new Promise((resolve) => {
         setTimeout(async () => {
             console.log('ЗАПУСК ПАРСЕРА')
 
             let result = [];
 
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 10; i++) {
                 result = [...result, ...await autoScroll(page)]
 
                 try {
@@ -38,20 +38,41 @@ export const parse = async (browser, login, password, url) => {
                 }
             }
 
-            resolve({
-                logs: 'ПАРСЕР ЗАВЕРШЕН',
-                length: result.length,
-                array: result,
-            })
+            resolve(result)
         }, 2000)
     })
+
+    const res = []
+
+    console.log(`НАЙДЕНО ${href.length} записей`)
+
+    for (let i = 0; i < href.length; i++) {
+        if (i % 50 === 0 && i !== 0) {
+            console.log(`РАСПАРШЕНО ${i} записей`)
+        }
+        await page.goto(href[i]);
+
+        await page.waitForSelector('.mlr_cnt')
+
+        const text = await page.evaluate(async () => {
+            return document.querySelector('.mlr_cnt').innerText
+        })
+
+        res.push(text)
+    }
+
+    return {
+        logs: "ПАРСЕР ЗАВЕРШЕН",
+        length: res.length,
+        array: res
+    }
 }
 
 const autoScroll = async (page) => {
     return await page.evaluate(async () => {
         return await new Promise((resolve) => {
             let totalHeight = 0;
-            let distance = 1000;
+            let distance = 500;
 
             const timer = setInterval(() => {
                 let scrollHeight = document.body.scrollHeight;
@@ -59,11 +80,13 @@ const autoScroll = async (page) => {
                 totalHeight += distance;
 
                 if(totalHeight >= scrollHeight) {
-                    const arrNotesElem = document.querySelectorAll('.media-text_cnt_tx.emoji-tx.textWrap');
+                    const arrNotesElem = document.querySelectorAll('.media-text_a');
 
-                    const tempArr = Array.from(arrNotesElem).map((item) => {
-                        return item.textContent
-                    })
+                    const tempArr = Array.from(arrNotesElem).reduce((sum, item) => {
+                        sum.push(item.href)
+
+                        return sum
+                    }, [])
 
                     clearInterval(timer);
                     resolve(tempArr)
